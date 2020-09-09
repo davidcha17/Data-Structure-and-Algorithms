614bc70489c7e57fae7af43dc01b9705be1c24bf
David Cha <littlesadtea@gmail.com>
2020-09-09 10:47:32 -0400

    heaps

--- udemy/LogarithmicRuntime/BinaryTree.js
+++ udemy/LogarithmicRuntime/BinaryTree.js
@@ -61,6 +61,62 @@ class BinarySearchTree {
         return current
     }
 
+    bfs() {
+        // iteratively 
+        let data = []
+        let queue = []
+        let node = this.root
+
+        queue.push(node) 
+        while(queue.length) {
+            node = queue.shift()
+            data.push(node)
+
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        return data
+    }
+
+    preOrder() {
+        // recursively
+        let data = []
+        let current = this.root 
+        function traverse(node) {
+            data.push(node) 
+            if(node.left) traverse(node.left)
+            if(node.right) traverse(node.right)
+        }
+        traverse(current)
+        return data
+    }
+
+    postOrder() {
+        // recursively
+        let data = []
+        let current = this.root
+        function traverse(node) {
+            if(node.left) traverse(node.left) 
+            if(node.right) traverse(node.right)
+            data.push(node)
+        }
+        traverse(current) 
+        return data
+    }
+
+    inOrder() {
+        // recursively
+        let data = [] 
+        let current = this.root 
+        function traverse(node) {
+            if(node.left) traverse(node.left)
+            data.push(node)
+            if(node.right) traverse(node.right)
+        }
+        traverse(current)
+        return data
+    }
+
 }
 
 let BST = new BinarySearchTree()
---
+++ udemy/LogarithmicRuntime/Heaps.js
@@ -0,0 +1,5 @@
+class MaxBinaryHeaps {
+    constructor() {
+        this.values = []
+    }
+}
