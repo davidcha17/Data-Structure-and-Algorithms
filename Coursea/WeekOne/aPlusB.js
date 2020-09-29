71b261cd8f8a7e46c5761dc7d62c8bddf1c404df
David Cha <littlesadtea@gmail.com>
2020-09-28 21:34:27 -0400

    coursea course

---
+++ Coursea/WeekOne/aPlusB.js
@@ -0,0 +1,8 @@
+// the task is to create a program that takes in two digits and adds the sum from the 
+// standard input
+
+function aPlusB(intOne, intTwo) {
+    return intOne + intTwo
+}
+
+aPlusB(2, 4)
