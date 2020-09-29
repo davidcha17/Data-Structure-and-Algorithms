288a53707bace22495eab3e83598859510ea1711
David Cha <littlesadtea@gmail.com>
2020-09-28 21:41:45 -0400

    go to coursea file and add test function

--- Coursea/WeekOne/aPlusB.js
+++ Coursea/WeekOne/aPlusB.js
@@ -1,8 +1,27 @@
 // the task is to create a program that takes in two digits and adds the sum from the 
 // standard input
 
-function aPlusB(intOne, intTwo) {
-    return intOne + intTwo
+var readline = require('readline');
+
+process.stdin.setEncoding('utf8');
+var rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false
+});
+
+rl.on('line', readLine);
+
+function readLine (line) {
+  if (line !== "\n") {
+    var a = parseInt(line.toString().split(' ')[0], 10);
+    var b = parseInt(line.toString().split(' ')[1], 10);
+    console.log(a + b);
+    process.exit();
+  }
+}
+
+function aPlusB(a, b) {
+    return a + b
 }
 
 aPlusB(2, 4)
