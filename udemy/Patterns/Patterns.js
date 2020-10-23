2bf784ce77e2c83d8b73472b0a3983702b4ede39
David Cha <littlesadtea@gmail.com>
2020-10-22 22:31:28 -0400

    more patterns

--- udemy/Patterns/Patterns.js
+++ udemy/Patterns/Patterns.js
@@ -48,3 +48,81 @@ function properSame(arr1, arr2) {
         }
     }
 }
+
+// multiple pointers 
+
+function isZero(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    // creating pointers for the first and last index of the array
+    while( left < right ) {
+        let sum = arr[left] + arr[right]; 
+    //  iterating the array with a while loop and adding the indexs from 
+    // both sides of the array
+    if(sum === 0) {
+        return arr[left], arr[right]
+    //  if the array is 0, return the elements that made sum 0 
+    } else if(sum > 0) {
+      right --;
+    } else {
+        left++;
+    // if the sum is greater than 0 we go down 1 from the right
+    // else we will go up 1 from the left 
+    }
+   }
+}
+
+// Sliding Window
+
+function maxSubArraySum(arr, num) {
+    let tempSum = 0;
+    let maxSum = 0;
+    // creating a place holder for the max sum and a sum
+    // that will constantly change during the iteration
+    if(arr.length < num) return null
+    // edge case
+    for(let i = 0; i < num; i++) {
+        maxSum += arr[i];
+        // console.log("maxSum", maxSum)
+    // we start at index 0 and up to the num 
+    // this is our window an we will keep adding on
+    // to the maxSum think of this as initial window
+    }
+        tempSum = maxSum
+        // giving the tempSum a value so we can compare other 
+        // sums throughout the iteration
+    for(let i = num; i < arr.length; i++) {
+        tempSum += arr[i] - arr[i - num];
+        // starting from the num (start of the window) 
+        // we will keep adding on to the variable
+        maxSum = Math.max(maxSum, tempSum);
+        // comparing which variable is greater
+    }
+    return maxSum 
+}
+
+// Divide and Conquer 
+
+function search(arr, val) {
+    let min = 0;
+    let max = arr.length - 1;
+    // Variables for the start and end of the array
+    while(min <= max) {
+        let middle = Math.floor((min + max) / 2);
+        let currElement = arr[middle];
+    // We are creating the middle index by dividing the start 
+    // and end of the array. Once we get the middle index
+    // we assign the index value to the variable 
+    console.log("before condition", middle)
+        if(arr[middle] < val) {
+            min = middle + 1;
+        } else if(arr[middle] > val) {
+            max = middle - 1;
+        } else {
+            return middle;
+        }
+        console.log("after condition", middle)
+    }
+    return -1;
+}
+
